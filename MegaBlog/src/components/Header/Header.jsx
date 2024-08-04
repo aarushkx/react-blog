99b268a9a3a645c4c4a28e2b1d0b17c59ba7301e
Aarush Kumar <2005aarushkumar@gmail.com>
2024-08-04 23:52:51 +0530

    Added few components

--- MegaBlog/src/components/Header/Header.jsx
+++ MegaBlog/src/components/Header/Header.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Container, Logo, LogoutButton } from "../index";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { logout } from "../../store/authSlice";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
-  return (
-    <div>
-      Header
-    </div>
-  )
+    const authStatus = useSelector((state) => state.auth.status);
+    const navigate = useNavigate();
+
+    const navItems = [
+        { name: "Home", path: "/", active: true },
+        { name: "Login", path: "/login", active: !authStatus },
+        { name: "Signup", path: "/signup", active: !authStatus },
+        { name: "All Posts", path: "/all-posts", active: authStatus },
+        { name: "Add Post", path: "/add-post", active: authStatus },
+    ];
+
+    return (
+        <div className="py-3 shadow bg-gray-500">
+            <Container>
+                <nav className="flex">
+                    <div className="mr-4">
+                        <Link to="/">
+                            <Logo width="70px" />
+                        </Link>
+                    </div>
+                    <ul className="flex ml-auto">
+                        {navItems.map((item) =>
+                            item.active ? (
+                                <li key={item.name}>
+                                    <button
+                                        onClick={() => navigate(item.slug)}
+                                        className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                                    >
+                                        {item.name}
+                                    </button>
+                                </li>
+                            ) : null
+                        )}
+                        {authStatus && (
+                            <li>
+                                <LogoutButton />
+                            </li>
+                        )}
+                    </ul>
+                </nav>
+            </Container>
+        </div>
+    );
 }
 
-export default Header
+export default Header;
--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -12,5 +12,8 @@
 
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutButton from "./Header/LogoutButton";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, LogoutButton };
